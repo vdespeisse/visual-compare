74c0fe25217bed5890064d3f48d27ee1347a6323
Vincent <v.despeisse@gmail.com>
2024-01-18 20:20:44 +0100

    fix: cleanup

--- libs/image-diff/src/lib/image-diff.spec.ts
+++ libs/image-diff/src/lib/image-diff.spec.ts
@@ -1,15 +1,16 @@
 import { imageDiff } from './image-diff'
 import * as path from 'path'
+import * as fs from 'fs'
 
 describe('imageDiff', () => {
   it('should work', async () => {
-    console.log('dqs', __dirname)
     const imageAPath = path.join(__dirname, '..', '..', 'tests', 'imageA.jpeg')
     const imageBPath = path.join(__dirname, '..', '..', 'tests', 'imageB.jpeg')
     const imageCPath = path.join(__dirname, '..', '..', 'tests', 'imageC.jpeg')
-    expect(
-      await imageDiff(imageAPath, imageCPath, path.join(__dirname, '..', '..', 'tests', 'tmp', 'diff.png')),
-    ).toEqual(false)
-    // expect(await imageDiff(imageAPath, imageBPath, '../../tests/tmp/output.jpg')).toEqual(true)
-  }, 100000)
+    const diffImagePath = path.join(__dirname, '..', '..', 'tests', 'tmp', 'diff.png')
+    const nonEqual = await imageDiff(imageAPath, imageCPath, diffImagePath)
+    expect(nonEqual).toEqual(false)
+    expect(fs.existsSync(diffImagePath)).toEqual(true)
+    expect(await imageDiff(imageAPath, imageBPath)).toEqual(true)
+  }, 10000)
 })
--- libs/image-diff/src/lib/image-diff.ts
+++ libs/image-diff/src/lib/image-diff.ts
@@ -1,28 +1,21 @@
 import * as fs from 'fs/promises'
-import looksSame from 'looks-same'
+import looksSame, { CreateDiffOptions } from 'looks-same'
 export async function compareImages(sourceImage: Buffer, targetImage: Buffer): Promise<boolean> {
   const { equal } = await looksSame(sourceImage, targetImage)
   return equal
 }
-// export function compareImages(sourceImage: Buffer, targetImage: Buffer, options: any): Promise<any> {
-//   return new Promise((resolve, reject) =>
-//     compare(sourceImage, targetImage, options, (err: any, data: any) => {
-//       if (err) {
-//         reject(err)
-//       }
-//       resolve(data)
-//     }),
-//   )
-// }
 
-export async function imageDiff(sourcePath: string, targetPath: string, outputPath?: string): Promise<number> {
+export async function imageDiff(
+  sourcePath: string,
+  targetPath: string,
+  outputPath?: string,
+  diffOptions?: CreateDiffOptions,
+): Promise<boolean> {
   const sourceImage = await fs.readFile(sourcePath)
   const targetImage = await fs.readFile(targetPath)
-  console.log('sourceImage', sourceImage)
   const equal = await compareImages(sourceImage, targetImage)
   if (outputPath && !equal) {
-    console.log('is not equal')
-    await looksSame.createDiff({
+    const defaultDiffOptions: CreateDiffOptions = {
       reference: sourceImage,
       current: targetImage,
       diff: outputPath,
@@ -32,12 +25,11 @@ export async function imageDiff(sourcePath: string, targetPath: string, outputPa
       antialiasingTolerance: 0,
       ignoreAntialiasing: true, // ignore antialising by default
       ignoreCaret: true, // ignore caret by default
+    }
+    await looksSame.createDiff({
+      ...defaultDiffOptions,
+      ...diffOptions,
     })
   }
   return equal
-  // return 0
-}
-
-export async function imageDiffWithThreshold(): Promise<number> {
-  return 0
 }
